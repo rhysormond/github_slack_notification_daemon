1fe727aad0e458b91b1b462ce7b99a40787dc2c3
rhys <rhysormond@gmail.com>
2019-12-12 14:10:41 -0800

    Add more explicit error messages
    Make it clearer which env vars are missing as well as more explicit
    handling of failed requests.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate reqwest;
 use std::{env, thread, time};
 
 use chrono::{DateTime, Local};
-use reqwest::blocking::{Client, Response};
+use reqwest::blocking::Client;
 
 use crate::github_notification::{GithubNotification, HasHtmlUrl};
 use crate::slack_message::SlackMessage;
@@ -19,9 +19,10 @@ const POLLING_FREQUENCY: time::Duration = time::Duration::from_secs(60);
 
 fn main() {
     let client: Client = Client::new();
-    let github_username: String = env::var("GITHUB_USERNAME").unwrap();
-    let github_token: String = env::var("GITHUB_TOKEN").unwrap();
-    let slack_hook: String = env::var("SLACK_HOOK").unwrap();
+    let github_username: String =
+        env::var("GITHUB_USERNAME").expect("Expected a GITHUB_USERNAME env var.");
+    let github_token: String = env::var("GITHUB_TOKEN").expect("Expected a GITHUB_TOKEN env var.");
+    let slack_hook: String = env::var("SLACK_HOOK").expect("Expected a SLACK_HOOK env var.");
 
     let mut last_notification_time: DateTime<Local> = Local::now();
 
@@ -37,32 +38,42 @@ fn main() {
     loop {
         last_notification_time = Local::now();
 
-        let response: Response = client
-            .get(GITHUB_API_URL)
-            .basic_auth(&github_username, Some(&github_token))
-            .query(&[("since", last_notification_time.to_rfc3339())])
-            .send()
-            .unwrap();
-        println!("Got response: {:?}.", response);
+        let notifications: Vec<GithubNotification> = {
+            let maybe_response = client
+                .get(GITHUB_API_URL)
+                .basic_auth(&github_username, Some(&github_token))
+                .query(&[("since", last_notification_time.to_rfc3339())])
+                .send();
 
-        let notifications: Vec<GithubNotification> =
-            response.json::<Vec<GithubNotification>>().unwrap();
+            match maybe_response {
+                Ok(response) => {
+                    println!("Got response to notification request: {:?}.", response);
+                    response.json::<Vec<GithubNotification>>().unwrap()
+                }
+                Err(error) => panic!("Failed to query the GitHub notification API: {:?}.", error),
+            }
+        };
         println!("Got notifications: {:?}.", notifications);
 
         let messages: Vec<SlackMessage> = notifications
             .iter()
             .map(|notification| {
-                let with_html_url = client
+                let maybe_response = client
                     .get(&notification.subject.url)
                     .basic_auth(&github_username, Some(&github_token))
-                    .send()
-                    .unwrap()
-                    .json::<HasHtmlUrl>()
-                    .unwrap();
-                println!("Got slack html url {:?}.", with_html_url);
+                    .send();
+
+                let with_html_url = match maybe_response {
+                    Ok(response) => {
+                        println!("Got response to html url request: {:?}.", response);
+                        response.json::<HasHtmlUrl>().unwrap()
+                    }
+                    Err(error) => panic!("Failed to query the GitHub for html url: {:?}.", error),
+                };
                 SlackMessage::from_github_notification(notification, with_html_url)
             })
             .collect();
+        println!("Got slack messages: {:?}.", messages);
 
         messages.iter().for_each(|payload| {
             println!("Sending slack message {}", serde_json::json!((payload)));
