a3866f9eba81f7c1c0fdd8412a354bcf3c870d4c
Rhys Ormond <rhysormond@gmail.com>
2019-12-14 12:09:39 -0800

    Add documentation for clients/messages (#8)
    Add some documentation for the github and slack clients/messages.
    There're some fields omitted from the github notification
    documentation since it's not immediately clear what those fields are
    used for.

--- src/github_client.rs
+++ src/github_client.rs
@@ -6,6 +6,13 @@ use reqwest::Error;
 /// https://developer.github.com/v3/activity/notifications/
 const GITHUB_API_URL: &str = "https://api.github.com/notifications";
 
+/// A client to get messages from the github notification API
+///
+/// # Arguments
+///
+/// * `client` - The reqwests client that gets from the github API
+/// * `username` - A github username associated with the token
+/// * `token` - A github personal access token with notification, repo, and non-admin read scopes
 pub struct GithubClient {
     client: Client,
     username: String,
@@ -21,6 +28,7 @@ impl GithubClient {
         }
     }
 
+    /// Gets all notifications since a timestamp even if they've been read
     fn get_notifications(&self, since: DateTime<Local>) -> Result<Response, Error> {
         self.client
             .get(GITHUB_API_URL)
@@ -29,6 +37,7 @@ impl GithubClient {
             .send()
     }
 
+    /// Gets a response from an API URL
     fn get(&self, api_url: &str) -> Result<Response, Error> {
         self.client
             .get(api_url)
@@ -36,12 +45,14 @@ impl GithubClient {
             .send()
     }
 
+    /// Gets an HTML URL from a notification's API URL and bundles them together
     fn get_html_url(&self, notification: GithubNotification) -> Result<NotificationWithUrl, Error> {
         let response = self.get(&notification.subject.url)?;
         let url: HasHtmlUrl = response.json::<HasHtmlUrl>()?;
         Ok(NotificationWithUrl::new(notification, url))
     }
 
+    /// Gets all notifications since a timestamp and bundles them with a human-usable HTML URL
     pub fn fetch_notifications(
         &self,
         since: DateTime<Local>,
